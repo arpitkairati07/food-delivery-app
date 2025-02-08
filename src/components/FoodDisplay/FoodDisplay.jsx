069435c4eb7cd27d3671d5faeeb8479334fb1f22
arpitkairati2002 <arpitkairati54@gmail.com>
2025-02-08 20:21:52 +0000

    Filter Property in Food Display

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,4 +1,3 @@
-// src/components/FoodDisplay/FoodDisplay.js
 import React, { useContext } from "react";
 import "./FoodDisplay.css";
 import { StoreContext } from "../../context/StoreContext";
@@ -7,14 +6,19 @@ import FoodItem from "../FoodItem/FoodItem";
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
 
+  // Filter food items based on the selected category
+  const filteredFoodList = food_list.filter((item) => {
+    return category === "All" || item.category === category;
+  });
+
   return (
     <div className="food-display" id="food-display">
       <h2>Top Dishes near you</h2>
       <div className="food-display-list">
-        {food_list.map((item, index) => (
+        {filteredFoodList.map((item, index) => (
           <FoodItem
-            key={index}
-            _id={item._id} 
+            key={index} // Use index as the key (or use item._id for a unique key)
+            _id={item._id}
             name={item.name}
             description={item.description}
             price={item.price}
