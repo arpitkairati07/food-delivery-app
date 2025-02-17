54edbb63a2a33b6d63bfe2a51112f74050f31b74
arpitkairati2002 <arpitkairati54@gmail.com>
2025-02-17 18:15:55 +0000

    changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config(); // Load environment variables
+dotenv.config(); 
 
 const connectDB = async () => {
     try {
-        // Remove useNewUrlParser and useUnifiedTopology options
+        
         await mongoose.connect(process.env.MONGO_URI);
         console.log("✅ MongoDB connection successful");
     } catch (error) {
         console.error("❌ MongoDB connection error:", error);
-        process.exit(1); // Stop the app if the connection fails
+        process.exit(1); 
     }
 };
 
