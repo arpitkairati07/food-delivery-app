d238d9322010fecabce4c6ce680bde7d2db29324
arpitkairati2002 <arpitkairati54@gmail.com>
2025-02-21 09:39:51 +0000

    login and signup functionality for the user

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -2,12 +2,13 @@ import React, {  useContext, useState } from 'react'
 import './LoginPopup.css'
 import { assets } from '../../assets/assets';
 import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
 
 
 
 const LoginPopup = ({setShowLogin}) => {
 
-    const {url} = useContext(StoreContext);
+    const {url,setToken} = useContext(StoreContext);
     const[currState,setCurrState]=useState("Login");
     const[data,setData]= useState({
         name:"",
@@ -23,7 +24,21 @@ const LoginPopup = ({setShowLogin}) => {
 
     const onLogin = async (event) =>{
         event.preventDefault()
-        
+        let newUrl = url;
+        if(currState == "Login"){
+            newUrl += "/api/user/login"
+        }else{
+            newUrl += "/api/user/register"
+        }
+        const response=await axios.post(newUrl,data);
+        if(response.data.success){
+            setToken(response.data.token)
+            localStorage.setItem("token",response.data.token);
+            setShowLogin(false);
+        }
+        else{
+            alert(response.data.message);
+        }
     }
       
   return (
--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -7,6 +7,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const url="http://localhost:5000"
+  const[token,setToken] = useState("");
 
   // Add an item to the cart
   const addToCart = (itemId) => {
@@ -44,7 +45,9 @@ const StoreContextProvider = (props) => {
     addToCart,
     removeFromCart,
     getTotalCartAmount,
-    url
+    url,
+    token,
+    setToken
   };
 
   return (
