277064844c81d13600ea55ba10b5424b0d692493
arpitkairati2002 <arpitkairati54@gmail.com>
2025-02-21 18:58:57 +0000

    get cart data functionality

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -46,7 +46,14 @@ const removeFromCart = async(req,res) =>{
 
 // Fetch User Cart data
 const getCart =async(req,res) =>{
-
+    try {
+        let userData = await userModel.findById( req.body.userId  );
+        let cartData = await userData.cartData;
+        res.json({ success: true, cartData })
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Server Error" });
+    }
 }
 
 export{addToCart,removeFromCart,getCart}
