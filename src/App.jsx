eb43735783e0808d1406c1b28d1eff0077eef773
arpitkairati2002 <arpitkairati54@gmail.com>
2025-02-23 12:22:17 +0000

    Cart Page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
 import Footer from './components/Footer/Footer.jsx'
 import LoginPopup from './components/LoginPopup/LoginPopup.jsx'
 import Verify from './pages/Verify/Verify.jsx'
+import MyOrders from './pages/MyOrders/MyOrders.jsx'
 
 const App = () => {
 const[showLogin,setShowLogin]=useState(false); 
@@ -21,8 +22,7 @@ const[showLogin,setShowLogin]=useState(false);
         <Route path='/cart' element={<Cart />}></Route>
         <Route path='/order' element={<PlaceOrder />}></Route>
         <Route path='/verify' element={<Verify />}></Route>
-        {/* <Route path='/login' element={<Login />}></Route>
-        <Route path='/signup' element={<Signup />}></Route> */}
+        <Route path='/myorders' element={<MyOrders />}></Route>
         '
       </Routes>
     </div>
