868293642f28ff548ee5af9892e26d75432ce123
arpitkairati2002 <arpitkairati54@gmail.com>
2025-02-23 06:28:10 +0000

    payment integration

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Cart from './pages/Cart/Cart.jsx'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
 import Footer from './components/Footer/Footer.jsx'
 import LoginPopup from './components/LoginPopup/LoginPopup.jsx'
+import Verify from './pages/Verify/Verify.jsx'
 
 const App = () => {
 const[showLogin,setShowLogin]=useState(false); 
@@ -19,6 +20,10 @@ const[showLogin,setShowLogin]=useState(false);
         <Route path='/' element={<Home />}></Route>
         <Route path='/cart' element={<Cart />}></Route>
         <Route path='/order' element={<PlaceOrder />}></Route>
+        <Route path='/verify' element={<Verify />}></Route>
+        {/* <Route path='/login' element={<Login />}></Route>
+        <Route path='/signup' element={<Signup />}></Route> */}
+        '
       </Routes>
     </div>
     <Footer></Footer>
