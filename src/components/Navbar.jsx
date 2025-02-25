60366766abda98d3610af28189de5306b61cdf15
arpitkairati2002 <arpitkairati54@gmail.com>
2025-02-25 19:21:59 +0000

    order button functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = ({setShowLogin}) => {
             : <div className='navbar-profile'>
                 <img src={assets.profile_icon} alt="Profile Icon" />
                 <ul className="nav-profile-dropdown">
-                  <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+                  <li onClick={()=>navigate("/myorders")}><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
                   <hr />
                   <li onClick={logOut}><img src={assets.logout_icon} alt="LogOut-Icon" /><p>LogOut</p></li>
                 </ul>
