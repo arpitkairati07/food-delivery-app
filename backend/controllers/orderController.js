90914f92ab73e03f9014ef972b5335126e2b782e
arpitkairati2002 <arpitkairati54@gmail.com>
2025-02-25 19:26:41 +0000

    api made to fetch all orders in admin pannel

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -81,4 +81,15 @@ const userOrder = async(req,res)=>{
         res.json({success:false,message:"Error"})
     }
 }
-export { placeOrder,verifyOrder,userOrder };
+
+// Listing Orders for the admin Pannel
+const listOrders = async(req,res) =>{
+    try {
+        const orders = await orderModel.find({});
+        res.json({success:true,data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"});
+    }
+}
+export { placeOrder,verifyOrder,userOrder, listOrders };
