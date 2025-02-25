5c735c2eabc926f315082afd43a7e6ce67081371
arpitkairati2002 <arpitkairati54@gmail.com>
2025-02-25 19:53:10 +0000

    Admin order page

--- admin/admin/src/pages/Orders/Orders.jsx
+++ admin/admin/src/pages/Orders/Orders.jsx
@@ -1,10 +1,61 @@
-import React from 'react'
-import './Orders.css'
+import React, { useEffect, useState } from 'react';
+import './Orders.css';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from 'axios';
+import { assets } from '../../../../../src/assets/assets';
 
-const Orders = () => {
+const Orders = ({ url }) => {
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrder = async () => {
+    try {
+      const response = await axios.get(url + "/api/order/list");
+      if (response.data.success) {
+        setOrders(response.data.data);
+        toast.success(response.data.message);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error("Failed to fetch orders");
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllOrder();
+  }, []);
+
   return (
-    <div>Orders</div>
-  )
-}
+    <div className='order add'>
+      <h3>Order Page</h3>
+      <div className="order-list">
+        {orders.map((order, index) => (
+          <div key={index} className="order-item">
+            <img src={assets.parcel_icon} alt="Parcel Icon" />
+            <div>
+              <p className="order-item-food">
+                {order.items.map((item, index) => 
+                  index === order.items.length - 1
+                    ? `${item.name} x ${item.quantity}`
+                    : `${item.name} x ${item.quantity}, `
+                )}
+              </p>
+              <p className='order-item-name'>{order.address.firstName+" "+order.address.lastName}</p>
+              <div className="order-item-address">
+                <p>{order.address.street+" ,"}</p>
+                <p>{order.address.city+" ,"+order.address.state+" ,"+order.address.country+" ,"+order.address.zipcode+" ,"}</p>
+              </div>
+              <p className="order-item-phone">{order.address.phone}</p>
+            </div>
+            <p>Items : {order.items.length}</p>
+            <p>Total Amount : ${order.amount}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Orders
+export default Orders;
